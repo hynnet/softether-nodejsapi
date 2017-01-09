5848081fc2d30f0629fe7fd4255a40454319cdda
Matias Sosa <matiassosas@gmail.com>
2017-01-09 11:58:58 -0300

    Clean code and add SessinList function

--- functions.js
+++ functions.js
@@ -5,63 +5,60 @@ var exec =  require("child_process").execSync;
 //********************************************************************************
 module.exports = {
 
+  sessionList: function(hub) {
+    var info = [];
+    var sessions = [];
+    var temp = [];
+    info = exec(CONNECTION + hub + " /cmd SessionList").toString().split("\n");
+    info = info.splice(14);
+    for (var i = 0 ; i < info.length-8;) {
+      for (var j = 0 ; j < 9; j++) {
+        temp.push(info[i+j].substring(17));       
+      }
+      
+      temp[0] = temp[4];
+      if (temp[3] !== "SecureNAT Session" && temp[0] !== "L3SW_ro") { // Don't include stystem connections
+        temp.splice(4,1); //Remove duplicated username in [4]
+        sessions.push(temp);
+      }
+      temp = [];
+      i=i+j;
+    }
+    return JSON.stringify(sessions);
+  },
+
+  IpTable: function(hub) {
+    var info = [];
+    var sessions = [];
+    var temp = [];
+    info = exec(CONNECTION + hub + " /cmd IpTable").toString().split("\n");   
+    info = info.splice(14);
+    for (i = 0 ; i < info.length-9;) {
+      for (j = 0 ; j < 7; j++) {
+        temp.push(info[i+j].substring(13));       
+      }
+      temp[3] = temp[3].replace(" (DHCP)",""); // Remove DHCP
+      temp[0] = temp[2].split("-")[1].toLowerCase(); // Build user name
+      temp[6] = temp[6].replace("On ","");
+      sessions.push(temp); 
+      temp = [];
+      i=i+j;      
+    }
+    return JSON.stringify(sessions);
+  },
+
   getConnections: function(hub) {
-    var sesiones = [];
+    var sessions = [];
     var temp = [];
     var info = [];
 
     if(hub == "servers") {
-      info = exec(CONNECTION + hub + " /cmd SessionList").toString().split("\n");
-      info = info.splice(14);
-      for (var i = 0 ; i < info.length-8;) {
-        for (var j = 0 ; j < 9; j++) {
-          temp.push(info[i+j].substring(17));       
-        }
-        
-        temp[0] = temp[4];
-        if (temp[3] !== "SecureNAT Session" && temp[0] !== "L3SW_ro") { // Don't include stystem connections
-          temp.splice(4,1); //Remove duplicated username in [4]
-          sesiones.push(temp);
-        }
-        temp = [];
-        i=i+j;
-        
-      }
-      
+      sessions = module.exports.sessionList(hub);
     }
     else {
-      info = exec(CONNECTION + hub + " /cmd IpTable").toString().split("\n");  	
-      info = info.splice(14);
-    
-      console.log(info);
-
-      for (i = 0 ; i < info.length-9;) {
-        for (j = 0 ; j < 7; j++) {
-          temp.push(info[i+j].substring(13));       
-        }
-        temp[3] = temp[3].replace(" (DHCP)",""); // Remove DHCP
-        temp[0] = temp[2].split("-")[1].toLowerCase(); // Build user name
-        temp[6] = temp[6].replace("On ","");
-        sesiones.push(temp);
-      
-      /*  // Get Origin IP
-        var sid = temp[2];
-        var ipOrg = [];
-        ipOrg = exec(CONNECTION + hub + " /cmd SessionGet " + sid).toString().split("\n");
-        ipOrg = ipOrg.splice(14);
-
-        if(ipOrg[43] === undefined) {
-          sesiones.splice(i,1);       // Don't show system account
-        }
-        else {
-          temp.push(ipOrg[1].substring(43));  //Local ipOrg[43], Public ipOrg[1]
-        }*/
-        // Continue ...  
-        temp = [];
-        i=i+j;      
-      }
+      sessions = module.exports.IpTable(hub);
     }
-    return JSON.stringify(sesiones); 
+    return sessions; 
   },
 
 
